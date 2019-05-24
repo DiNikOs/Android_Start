6455b721cd6075ce8488e51883496cc356e7a363
DiNikOs <demon22odn08@rambler.ru>
2019-05-24 10:28:22 +0300

    HW2. It is good+

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -12,14 +12,12 @@ public class MenuScreen extends BaseScreen {
 
     private SpriteBatch batch;
     private Texture img;
+    private static float LEN = 2f;
 
     private Vector2 touch;
     private Vector2 v0;
     private Vector2 v;
-    private Vector2 v2;
-    private Vector2 v3;
     private Vector2 pos;
-    private Vector2 pos2;
 
     @Override
     public void show() {
@@ -28,11 +26,8 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("badlogic.jpg");
         touch = new Vector2();
         v0 = new Vector2();
-        v = new Vector2(0,0);
-        v2 = new Vector2();
-        v3 = new Vector2(0, 0);
+        v = new Vector2();
         pos = new Vector2();
-        pos2 = new Vector2();
     }
 
     @Override
@@ -43,17 +38,14 @@ public class MenuScreen extends BaseScreen {
         batch.begin();
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if (!touch.isZero()) {
-            v0.add(v3);
-            if (v0.len() < pos2.len()) {
-                pos.add(v3);
-           }
-            else {
-                touch.setZero();
-            }
+        v0.set(touch);
+        if (v0.sub(pos).len() <= LEN) {
+            pos.set(touch);
+        }
+        else {
+            pos.add(v);
         }
     }
-
 
     @Override
     public void dispose() {
@@ -66,15 +58,8 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         super.touchDown(screenX, screenY, pointer, button);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v0.set(0,0);
-        pos2.set(0,0);
-        v.set(pos.x, pos.y);
+        v.set(touch.cpy().sub(pos)).setLength(LEN);
         System.out.println("pos.x= " + pos.x + "pos.y= " + pos.y);
-        v2.set(touch.x, touch.y);
-        v3 = v2.sub(v);
-        pos2.add(v3);
-        v.nor();
-        v2.nor();
         return false;
     }
 
