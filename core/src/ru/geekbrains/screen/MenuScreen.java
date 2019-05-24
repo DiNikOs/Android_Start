1edc782be7eb47ad02c0a3e66ab3a0b6870e7a3a
DiNikOs <demon22odn08@rambler.ru>
2019-05-24 11:07:36 +0300

    HW3. Not good.Basis

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -4,15 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.BadLogic;
 
 public class MenuScreen extends BaseScreen {
 
-    private SpriteBatch batch;
-    private Texture img;
+//    private SpriteBatch batch;
+//    private Texture img;
+
+    private Texture bg;
+    private Texture badLogicTexture;
+    private Background background;
+    private BadLogic badLogic;
+
     private static float LEN = 2f;
+
 
     private Vector2 touch;
     private Vector2 v0;
@@ -22,8 +33,13 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+//        batch = new SpriteBatch();
+//        img = new Texture("badlogic.jpg");
+        bg = new Texture("bg.jpg");
+        background = new Background(new TextureRegion(bg));
+        badLogicTexture = new Texture("badlogic.jpg");
+        badLogic = new BadLogic(new TextureRegion(badLogicTexture));
+
         touch = new Vector2();
         v0 = new Vector2();
         v = new Vector2();
@@ -36,7 +52,9 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.4f, 0.3f, 0.9f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        //batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        badLogic.draw(batch);
         batch.end();
         v0.set(touch);
         if (v0.sub(pos).len() <= LEN) {
@@ -49,18 +67,24 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+        //batch.dispose();
+        //img.dispose();
+        bg.dispose();
+        badLogicTexture.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        super.touchDown(screenX, screenY, pointer, button);
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(LEN);
-        System.out.println("pos.x= " + pos.x + "pos.y= " + pos.y);
-        return false;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        badLogic.resize(worldBounds);
+    }
+
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return super.touchDown(touch, pointer);
     }
 
 }
---
+++ core/src/ru/geekbrains/sprite/BadLogic.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.geekbrains.base.Sprite;
+import ru.geekbrains.math.Rect;
+
+public class BadLogic extends Sprite {
+
+    public BadLogic(TextureRegion region) {
+        super(region);
+        regions[frame].getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+    }
+
+    @Override
+    public void resize(Rect wordBounds) {
+        setHeightProportion(0.5f);
+    }
+}
