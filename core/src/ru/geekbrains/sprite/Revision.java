9a665d2ae42c1820d93dd30e356044b9227779cd
DiNikOs <demon22odn08@rambler.ru>
2019-06-11 19:02:46 +0300

    HW8. revision, revisionPool, NameGame

--- core/src/ru/geekbrains/sprite/Revision.java
+++ core/src/ru/geekbrains/sprite/Revision.java
@@ -9,28 +9,12 @@ import ru.geekbrains.pool.RevisionPool;
 
 public class Revision extends Sprite {
 
-    protected RevisionPool revisionPool;
-
-    protected Vector2 v;
-    protected Vector2 v0;
-
     protected Rect worldBounds;
 
-    protected float reloadInterval;
-    protected float reloadTimer;
-
-    protected int damage;
-    protected int hp;
-
     private MainShip mainShip;
-    private Revision revision;
 
-    public Revision(RevisionPool revisionPool, Rect worldBounds, Revision revision, MainShip mainShip) {
-        this.revisionPool = revisionPool;
+    public Revision(Rect worldBounds, MainShip mainShip) {
         this.worldBounds = worldBounds;
-        this.v = new Vector2();
-        this.v0 = new Vector2();
-        this.revision = revision;
         this.mainShip = mainShip;
     }
 
